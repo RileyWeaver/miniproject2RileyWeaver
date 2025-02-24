d9ffc5f7c6eeb1344e5a07ad67d6e31211fd5a7f
Riley Weaver <c0wman1928@live.com>
2025-02-23 19:24:26 -0500

    Created 2 graphs. 1st one being Tp 3 Gaming Teams sorted by the data of popular games and 2nd being distribution of ratings among all the popular games from the dataset.

--- main.py
+++ main.py
@@ -14,15 +14,63 @@ I,     Riley Weaver    , affirm that the work submitted for this assignment is e
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 import os
 
 os.makedirs("charts", exist_ok=True)
 
 games = pd.read_csv("games.csv", index_col=0)
 
+# top 3 Teams
 team_counts = games['Team'].value_counts()
 
-top_teams = team_counts.head(5)
+top_teams = team_counts.head(3)
 
-print("Top 5 game companies with the most popular games up to 2023: ")
+print("Top 3 Teams: ")
 print(top_teams)
+
+
+
+#Distribution of ratings
+ratings_amount = games['Rating'].value_counts()
+
+bins = np.arange(0, 5.0, 0.3)
+
+count = np.arange(0, 500, 50)
+
+print("Rating Distribution: ")
+print((ratings_amount))
+
+# For chart 3 Atlus games comparison
+
+
+
+
+
+# Chart 1: Top 3 Teams chart
+plt.figure(figsize = (8,6))
+
+color_list = ['red', 'green', 'blue']
+
+top_teams.plot(kind='bar', color=color_list)
+plt.title('Top 3 Teams By Amount Of Games Popular')
+plt.xlabel('Teams')
+plt.xticks(rotation=0)
+plt.ylabel('Frequency')
+
+
+# Chart 2: Distribution of ratings chart
+plt.figure(figsize = (8,6))
+plt.hist(games['Rating'], bins=10, color='orange', edgecolor='black')
+plt.xlabel('Rating')
+plt.xticks(bins)
+plt.ylabel('Count')
+plt.yticks(count)
+plt.title('Distribution of Ratings Among Popular Games')
+plt.show()
+
+# Chart 3: Atlus games comparison
+
+
+
+
